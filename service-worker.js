b9c0486a403f9e029b5eeda8e790689ff8a7b8c0
maosonglist <shutain17@gmail.com>
2026-03-07 13:51:16 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
-self.addEventListener('install', function(e) {
-  console.log('Service Worker installed');
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // いまは何もしないけど、PWA判定と将来の拡張用に残す
 });
